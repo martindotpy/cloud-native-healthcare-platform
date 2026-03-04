58442d6df0ac5d7b74d6ec310cf62c5b30870577
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2026-03-04 08:01:31 -0500

    feat: use axios instead of fetch

--- shared/typescript/openapi-ts.config.ts
+++ shared/typescript/openapi-ts.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from "@hey-api/openapi-ts"
 export default defineConfig({
   input: ["http://localhost:8080/api/appointment/openapi.json"],
   output: {
-    path: "src/client/api",
+    path: "src/api/client",
     postProcess: ["prettier"],
   },
   watch: true,
 
   plugins: [
     {
-      name: "@hey-api/client-fetch",
+      name: "@hey-api/client-axios",
       baseUrl: false,
     },
     "@tanstack/react-query",
